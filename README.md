2a771e0a069481837a7bc0b65b53340bfd8e4389
Rasujon3 <rasujon3@gmail.com>
2022-08-10 00:45:08 +0600

    Send HTML to Browser

---
+++ README.md
@@ -0,0 +1,3 @@
+1. node init
+2. app listen
+3. Send HTML to Browser
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello Node js, you are awesome</h1>`);
+});
+
 app.listen(4000, () => {
   console.log(`Server is listening on http://localhost:4000`);
 });
