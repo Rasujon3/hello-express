d3b652c8f42b4e95c4b8be9d87afa5bd80174629
Rasujon3 <rasujon3@gmail.com>
2022-08-10 00:57:23 +0600

    Understand the pipeline

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 1. node init
 2. app listen
 3. Send HTML to Browser
+4. Understand the pipeline
--- index.js
+++ index.js
@@ -9,3 +9,9 @@ app.get("/", (req, res) => {
 app.listen(4000, () => {
   console.log(`Server is listening on http://localhost:4000`);
 });
+
+function handler(req, res, next) {
+  // 1. read request object
+  // 2. process request
+  // 3. response back the result
+}
