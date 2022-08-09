f5b1c7c60db39e0e33b6e8ecf78cbc50bae66b4a
Rasujon3 <rasujon3@gmail.com>
2022-08-10 01:10:27 +0600

    Handle Multiiple Routes

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 2. app listen
 3. Send HTML to Browser
 4. Understand the pipeline
+5. Install Nodemon
+6. Handle Multiple Routes
--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Hello Node js, you are awesome</h1>`);
+  res.send(`<h1>I am Home Route</h1>`);
+});
+
+app.get("/about", (req, res) => {
+  res.send(`<h1>I am About Route</h1>`);
+});
+
+app.get("/help", (req, res) => {
+  res.send(`<h1>I am Help Route</h1>`);
 });
 
 app.listen(4000, () => {
